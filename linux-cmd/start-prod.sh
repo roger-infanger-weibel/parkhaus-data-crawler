d1695a9375080ea903ea95d882ba730a5bc3190c
rogers-code <roger@roil.ch>
2026-07-29 12:42:32 +0200

    Update start-prod.sh

--- linux-cmd/start-prod.sh
+++ linux-cmd/start-prod.sh
@@ -1,4 +1,5 @@
 pkill -f scheduler-prod.py 
-cd scanner-prod/
+cp -u latest-github/scanner/* scanner-prod
+cd scanner-prod
 cp scheduler.py scheduler-prod.py 
 nohup python3 -u scheduler-prod.py > scheduler-prod.log 2>&1 &
