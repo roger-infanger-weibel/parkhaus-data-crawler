5fa394c7dcfa166be8458ca616cc100c30402d69
rogers-code <roger@roil.ch>
2026-07-29 12:46:32 +0200

    Update start-test.sh

--- linux-cmd/start-test.sh
+++ linux-cmd/start-test.sh
@@ -1,4 +1,5 @@
 pkill -f scheduler-test.py 
+cp -u latest-github/scanner/* scanner-test
 cd scanner-test/
 cp scheduler.py scheduler-test.py 
 nohup python3 -u scheduler-prod.py > scheduler-test.log 2>&1 &
