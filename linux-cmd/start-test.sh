07da62f27ee5e6ebcfff27d5bdb35007e52bfd3c
rogers-code <roger@roil.ch>
2026-07-29 12:46:57 +0200

    Update start-test.sh

--- linux-cmd/start-test.sh
+++ linux-cmd/start-test.sh
@@ -2,4 +2,4 @@ pkill -f scheduler-test.py
 cp -u latest-github/scanner/* scanner-test
 cd scanner-test/
 cp scheduler.py scheduler-test.py 
-nohup python3 -u scheduler-prod.py > scheduler-test.log 2>&1 &
+nohup python3 -u scheduler-test.py > scheduler-test.log 2>&1 &
